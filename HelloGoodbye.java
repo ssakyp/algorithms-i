f95089fbd8937aebd5d7ae4b33377bf127334d75
Sultan Sakyp <103821526+ssakyp@users.noreply.github.com>
2025-12-14 11:02:03 +0000

    module-1

---
+++ HelloGoodbye.java
@@ -0,0 +1,7 @@
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        if (args == null || args.length < 2) return;
+        System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
+    }
+}
